334adc50e8b03abc82815b02dfa323983e2de1cf
francesco boldrin <francesco.boldrin@studenti.unitn.it>
2024-11-13 14:32:23 +0100

    improve graph class

--- utils/TMGraph.py
+++ utils/TMGraph.py
@@ -2,9 +2,17 @@
 Author: francesco boldrin francesco.boldrin@studenti.unitn.it
 Date: 2024-11-13 14:27:30
 LastEditors: francesco boldrin francesco.boldrin@studenti.unitn.it
-LastEditTime: 2024-11-13 14:29:55
+LastEditTime: 2024-11-13 14:32:10
 FilePath: utils/TMGraph.py
 Description: Contains the definition of the class Graph
+"""
+
+"""
+nodes = {
+            id: label (name, type)
+            ...
+        }
+edges = [[id1,id2, label],...]
 """
 
 class Graph:
