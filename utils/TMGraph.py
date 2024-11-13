5b817d369d74d3834e355d94767120b598246c9f
francesco boldrin <francesco.boldrin@studenti.unitn.it>
2024-11-13 14:30:23 +0100

    Create graph class

---
+++ utils/TMGraph.py
@@ -0,0 +1,19 @@
+"""
+Author: francesco boldrin francesco.boldrin@studenti.unitn.it
+Date: 2024-11-13 14:27:30
+LastEditors: francesco boldrin francesco.boldrin@studenti.unitn.it
+LastEditTime: 2024-11-13 14:29:55
+FilePath: utils/TMGraph.py
+Description: Contains the definition of the class Graph
+"""
+
+class Graph:
+    
+    def __init__(self):
+        self.nodes = {}
+        self.edges = []
+        self.num_of_nodes = 0
+        self.num_of_edges = 0
+    
+    
+    
