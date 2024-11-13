bb5984549edb6248f28af5fd085e8edf34091a16
Ice020829 <90243409+Ice020829@users.noreply.github.com>
2024-11-13 21:50:12 +0800

    Update TMGraph.py

--- utils/TMGraph.py
+++ utils/TMGraph.py
@@ -23,5 +23,25 @@ class Graph:
         self.num_of_nodes = 0
         self.num_of_edges = 0
     
+    def add_node(self, node_id, label, name, node_type):
+        #
+        self.nodes[node_id] = {'label': label, 'name': name, 'type': node_type}
+        self.num_of_nodes += 1
+    
+    def add_edge(self, id1, id2, label):
+        
+        self.edges.append([id1, id2, label])
+        self.num_of_edges += 1
+    
+    def display_graph(self):
+       
+        print("Nodes:")
+        for node_id, properties in self.nodes.items():
+            print(f"  {node_id}: {properties}")
+        
+        print("\nEdges:")
+        for edge in self.edges:
+            print(f"  {edge[0]} -- {edge[2]} --> {edge[1]}")
+    
     
     
